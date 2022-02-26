4b8ae81da7f46a16281fe26fdfc1b9850fc71306
CamagniR <rcama82@gmail.com>
2022-02-26 15:51:35 +0100

    Creazione Query
    create query in funzioni richiamabili da front-end

--- Web Service/prova.php
+++ Web Service/prova.php
@@ -1,19 +1,63 @@
 <?php
-include_once("config.php");	
+	
+	function getDataBase(){
+			
+		$sql_query = "SELECT * FROM prodotti  ";
+		getDati($sql_query);
 
-	$sql_query = "SELECT * FROM prodotti WHERE Categoria='Barbecue' ";
-	$resultset = mysqli_query($link, $sql_query) or die("database error:". mysqli_error($link));	
-	$products = array();
-	if(mysqli_num_rows($resultset)) {
-		while($product = mysqli_fetch_assoc($resultset)) {
-			$products[] = array('product'=>$product);
-		}
+	}	
+
+	function getCategoria($categoria){
+
+		$sql_query = "SELECT * FROM prodotti WHERE Categoria= '$categoria' ";
+		getDati($sql_query);
+
 	}
 
-	/* output result in required format */
-	
-		header('Content-type: application/json');
-		echo json_encode(array('products'=>$products));
+	function getAllCategorie(){
 		
-	mysqli_close($link);
+		$sql_query = "SELECT Categoria FROM prodotti ";
+		getDati($sql_query);
+
+	}
+
+	function getProductByID($id){
+		
+		$sql_query = "SELECT * FROM prodotti WHERE ID='$id'";
+		getDati($sql_query);
+
+	}
+
+	function getDati($sql_query){
+		include "config.php";
+	
+		$resultset = mysqli_query($link, $sql_query) or die("database error:". mysqli_error($link));	
+		$products = array();
+		if(mysqli_num_rows($resultset)) {
+			while($product = mysqli_fetch_assoc($resultset)) {
+				$products[] = array('product'=>$product);
+			}
+		}
+	
+		/* output result in required format */
+		
+			header('Content-type: application/json');
+			echo json_encode(array('products'=>$products));
+			
+		mysqli_close($link);
+
+	}
+
+
+
+	//getDataBase();
+	//getAllCategorie();
+	//getProductByID("3");
+
+
+
+
+
+
+
 ?>
