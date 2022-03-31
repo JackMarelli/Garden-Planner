34b38fd09024108984dbafa739889984a961a108
CamagniR <rcama82@gmail.com>
2022-03-31 12:53:06 +0200

    Risoluzione problemi
    Risolti tutti i problemi riguardo alla formattazione json delle query
    
    Co-Authored-By: EdoRusso <82151161+EdoRusso@users.noreply.github.com>

--- Web Service/config.php
+++ Web Service/config.php
@@ -1,14 +1,10 @@
 <?php
 
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_NAME','dbgardenplanner');
+// Create connection
+$conn = new mysqli("localhost", "root","","dbgardenplanner");
+// Check connection
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+} 
 
-$link = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME);
-
-if($link===false)
-{
-    die("errore".mysqli_connect_error());
-
-}
+?>
--- Web Service/prova.php
+++ Web Service/prova.php
@@ -1,66 +1,9 @@
 <?php
 	
 	function getDataBase(){
-		//$sql_query = "SELECT * FROM prodotti  ";
-		$sql_query = "SELECT ID,NomeProdotto,Categoria,InformazioniProdotti,Larghezza,Lunghezza,Costo,immagine FROM prodotti  ";
-		//getDati($sql_query);
-        getDatiII($sql_query);
-	}	
-
-	function getDatiII($sql_query){
-		include "config.php";
-
-		$res=mysqli_query($sql_query,MYSQLI_ASSOC);
-	    if($res && mysqli_num_rows($res)>0){
-			// qui ci andrà il codice per stampare i risultati
-			while($row=mysqli_fetch_assoc($res)){
-				echo $row['ID']." <br>";
-				echo $row['NomeProdotto']." <br>";
-				echo $row['Categoria']." <br> ";
-				echo $row['InformazioniProdotti']." <br> ";
-				echo $row['Larghezza']." <br> ";
-				echo $row['Lunghezza']." <br> ";
-				echo $row['Costo']." <br> ";
-				echo $row['immagine']." <br> ";
-			}
-		}
-		
-	}
-
-
-	function getInformazioni(){
-		include "config.php";
-		$sql_query = "SELECT InformazioniProdotti FROM prodotti  ";
-	
-	
-		$resultset = mysqli_query($link, $sql_query) or die("database error:". mysqli_error($link));	
-		$products = array();
-		if(mysqli_num_rows($resultset)) {
-			$i =0;
-			while($product = mysqli_fetch_assoc($resultset)) {
-				$info[$i]=$product;
-				$i++;
-			}
-		}
-
-		$i=0;
-		$risultato="{\"infos\":[ ";
-		while($i< count($info))
-		{
-			$risultato=$risultato.implode($info[$i]);
-			if($i!=count($info)-1){
-				$risultato=$risultato.",";
-			}
-			$risultato=$risultato."\n";
-			$i++;
-
-		}
-		$risultato=$risultato."]}";
-		echo  $risultato;
-			
-		//echo $products;
-		mysqli_close($link);
-
+		$sql_query = "SELECT * FROM prodotti  ";
+		getDatiAll($sql_query);
+        
 	}	
 
 	function getCategoria($categoria){
@@ -79,15 +22,15 @@
 
 	function getProductByID($id){
 		
-		$sql_query = "SELECT ID,NomeProdotto,Categoria,Larghezza,Lunghezza,Costo,immagine FROM prodotti WHERE ID='$id'";
-		getDati($sql_query);
+		$sql_query = "SELECT ID,NomeProdotto,Categoria,InformazioniProdotti,Larghezza,Lunghezza,Costo,immagine FROM prodotti WHERE ID='$id'";
+		getDatiAll($sql_query);
 
 	}
 
 	function getDati($sql_query){
 		include "config.php";
 	
-		$resultset = mysqli_query($link, $sql_query) or die("database error:". mysqli_error($link));	
+		$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));	
 		$products = array();
 		if(mysqli_num_rows($resultset)) {
 			while($product = mysqli_fetch_assoc($resultset)) {
@@ -101,16 +44,49 @@
 			header('Content-type: application/json');
 			echo json_encode(array('products'=>$products));
 			
-		mysqli_close($link);
+		mysqli_close($conn);
+
+	}
+
+	function getDatiAll($sql_query){
+		require_once("config.php");
+	
+		$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));	
+		$products = array();
+		if(mysqli_num_rows($resultset)) {
+			while($product = mysqli_fetch_assoc($resultset)) {
+				//print_r($product);
+
+				$myObj = new stdClass();
+				$myObj->id = $product['ID'];
+				$myObj->NomeProdotto = $product['NomeProdotto'];
+				$myObj->Categoria = $product['Categoria'];
+				$myObj->InformazioniProdotti = json_decode($product['InformazioniProdotti']);
+				$myObj->Larghezza = $product['Larghezza'];
+				$myObj->Lunghezza = $product['Lunghezza'];
+				$myObj->Costo = $product['Costo'];
+				$myObj->Immagine = $product['immagine'];
+
+				$products[] = array('product'=>$myObj);
+				
+			}
+		}
+	
+		/* output result in required format */
+		
+			header('Content-type: application/json');
+			echo json_encode(array('products'=>$products));
+			
+		mysqli_close($conn);
 
 	}
 
 
 	
-	getDataBase();
+	//getDataBase();
 	//getAllCategorie();
 	//getProductByID("3");
-	//getInformazioni();
+
 
 
 
