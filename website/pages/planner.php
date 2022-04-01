566a55163049ac1e25178aad23cf5d40d912dd9a
JackMarelli <marelligiacomo@gmail.com>
2022-04-01 12:52:32 +0200

    collapsible menu in planner.php

--- website/pages/planner.php
+++ website/pages/planner.php
@@ -6,6 +6,7 @@
     <title>PLANTERA | Planner</title>
     <link rel="stylesheet" href="../css/general.css">
     <link rel="stylesheet" href="../css/mybootstrap.css">
+    <script src="../js/animations.js"></script>
 </head>
 <body>
     <div class="container col">
@@ -14,11 +15,46 @@
         <span>Tutorial</span>
         <span>FAQ</span>
         <span>Contact</span>
-      </nav>
-      <div class="row">
-          <div class="col">Menu a tendine</div>
-          <div class="col">planner</div>
-      </div>
+    </nav>
+    <div class="row fg-1">
+            <div class="col left">
+                <button type="button" class="collapsible">Barbeque</button>
+                <div class="collapsible-content">
+                <ul>
+                    <li>
+                        <div class="product row">
+                            <div class="image"><img src="../../images/2_bbq.png" alt=""></div>
+                            <div class="text col">
+                                <div class="name">Barbeque 1 long nameeeeee</div>
+                                <div class="seemore">See more...</div>
+                                <div class="price">699€</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="product row">
+                            <div class="image"><img src="../../images/3_bbq.png" alt=""></div>
+                            <div class="text col">
+                                <div class="name">Barbeque 2 short</div>
+                                <div class="seemore">See more...</div>
+                                <div class="price">539€</div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                </div>
+                <button type="button" class="collapsible">Open Collapsible</button>
+                <div class="collapsible-content">
+                <ul>
+                    <li>BBQ1</li>
+                    <li>BBQ2</li>
+                    <li>BBQ3</li>
+                    <li>BBQ4</li>
+                </ul>
+                </div>
+            </div>
+        <div class="col right fg-1">planner</div>
+    </div>
     <?php
         //echo "x".$_GET["x"];
         //echo "y".$_GET["y"];
