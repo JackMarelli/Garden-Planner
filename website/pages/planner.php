60c401eeaad6ad670685536aad4f9ddcc7f13702
JackMarelli <marelligiacomo@gmail.com>
2022-04-28 13:58:21 +0200

    planner summary, details, dragndrop

--- website/pages/planner.php
+++ website/pages/planner.php
@@ -27,6 +27,7 @@
                                     <div class="name">Barbeque 1 long nameeeeee LOREM IPSUM DOLOR SIT AMET</div>
                                     <div onclick="openDetails(0)" class="seemore">See more...</div>
                                     <div class="price">699€</div>
+                                    <div class="btn cta-primary action">+ Aggiungi</div>
                                 </div>
                             </div>
                         </li>
@@ -37,6 +38,7 @@
                                     <div class="name">Barbeque 2 short</div>
                                     <div onclick="openDetails(1)" class="seemore">See more...</div>
                                     <div class="price">539€</div>
+                                    <div class="btn cta-primary action">+ Aggiungi</div>
                                 </div>
                             </div>
                         </li>
@@ -45,13 +47,16 @@
             </div>
             <div class="col right fg-1">
                 <h2 class="mt-3">Plan your garden here.</h2>
-                <div id="planner"></div>
+                <div id="planner">
+                    <div id="testRect" class="draggable">I'm a rectangle</div>
+                    <div id="testCirc" class="draggable">I'm a circle</div>
+                </div>
                 <div id="summaryOpen" class="btn cta-primary mb-3">Sommario</div>
             </div>
             <div id="summary">
                 <div id="summaryClose" class="close"><img src="../../images/elements/icons/icons8-close.svg" alt=""></div>
                 <div class="content">
-                    <h1 id="productName" class="m-3">Nome prodotto</h1>
+                    <h1 class="m-3">Sommario</h1>
                     <div class="list my-2 mx-3">
                         <div class="mx-3 prodotto">
                             <div class="action"><img src="../../images/elements/icons/icons8-close.svg" alt=""></div>
@@ -70,18 +75,19 @@
             <div id="details">
                 <div id="detailsClose" class="close"><img src="../../images/elements/icons/icons8-close.svg" alt=""></div>
                 <div class="content">
-                   <div class="prodotto">
-                       <img src="" alt="">
+                   <div class="m-3 prodotto">
+                       <img src="../../images/2_bbq.png" alt="errore">
                        <div class="text">
-                           <div id="productName" class="nome">Nome Prodotto</div>
-                           <div id="productDesc" class="desc">Lorem ipsum dolor sit amet</div>
-                           <div id="productPrice" class="prezzo">699€</div>
+                           <h1 id="detailsProductName" class="mx-3 nome">Nome Prodotto</h1>
+                           <div id="productDesc" class="m-3 desc">Lorem ipsum dolor sit amet</div>
+                           <div id="productPrice" class="mx-3 prezzo">699€</div>
                        </div>
                    </div>
-                   <div id="propTable" class="dettagli">Qui apparirà la tabella dei dettagli</div>
+                   <div id="propTable" class="prop">Qui apparirà la tabella dei dettagli</div>
                 </div>
             </div>
         </div>
+    <script src="https://unpkg.com/interactjs/dist/interact.min.js"></script>
     <script src="../js/animations.js"></script>
     <script src="../js/planner.js"></script>
 </body>
